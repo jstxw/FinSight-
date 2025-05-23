667d8a7a92afe09918146d30721582a622df881c
jstxw <ssivfe@Gmail.com>
2025-05-23 11:33:24 -0400

    added stuff

--- frontend/expense_tracker/src/App.jsx
+++ frontend/expense_tracker/src/App.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+import Login from "./pages/auth/Login.jsx";
+import SignUp from "./pages/auth/SignUp.jsx";
+import Home from "./pages/auth/Dashboard/home.jsx";
+import Income from "./pages/auth/Dashboard/Income.jsx";
+import Expense from "./pages/auth/Dashboard/Expense.jsx";
+
 const App = () => {
   return (
-    <div className='text-3xl text-purple-600'>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}/>
+          <Route path="/Login" exact element={<Login />}/>
+          <Route path="/SignUp" exact element={<SignUp />}/>
+          <Route path="/Home" exact element={<Home />}/>
+          <Route path="/Income" exact element={<Income />}/>
+          <Route path="/Expense" exact element={<Expense />}/>
+      </Routes>
+      </Router>
+
+    </div>
   )
-}
+} //This code helps a website decide what to show when someone goes to different parts, like the home page or the login page.
 
 export default App
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token"); 
+//This tries to get a token (a secret string) from the browser’s local storage. That token means: “this person is logged in.”
+
+  return isAuthenticated ? (
+    <Navigate to="/Dashboard"/>
+  ) :
+  ( 
+    <Navigare to="/Login" />
+  );
+};
--- frontend/expense_tracker/src/pages/auth/Login.jsx
+++ frontend/expense_tracker/src/pages/auth/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import AuthLayout from '../../components/AuthLayout'
 
 const Login = () => {
+  const [email, setEmail] = userState("");
+  const [password, setPassword] = userState("");
+  const [error, setError] = userState("");
   return (
-    <div>Login</div>
+    <AuthLayout>
+      <div className="lg: w-[70%] h-3/4 mid:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back!</h3>
+        <p className="text-xs text-slate-700 mt-1.5 mb-6 ">
+        Please enter your details to log in!</p>
+      </div>
+
+
+    </AuthLayout>
   )
 }
 
